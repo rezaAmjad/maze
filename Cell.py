93236bf96185b010c16c8e18f8aaeb9f9f7a1bfb
reza <reza.amjad65@gmail.com>
2021-04-29 09:55:34 -0700

    initial commit

---
+++ Cell.py
@@ -0,0 +1,43 @@
+import pygame
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+
+class Cell:
+
+    def __init__(self, row, col, width):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.width = width
+        self.walls = [True, True, True, True]
+        self.visited = False
+
+    def draw(self, win):
+
+        # top
+        if self.walls[0]:
+            pygame.draw.line(win, self.color, (self.x, self.y), (self.x + self.width, self.y))
+        # left
+        if self.walls[1]:
+            pygame.draw.line(win, self.color, (self.x, self.y), (self.x, self.y + self.width))
+        # bottom
+        if self.walls[2]:
+            pygame.draw.line(win, self.color, (self.x, self.y + self.width),
+                             (self.x + self.width, self.y + self.width))
+        # right
+        if self.walls[3]:
+            pygame.draw.line(win, self.color, (self.x + self.width, self.y),
+                             (self.x + self.width, self.y + self.width))
+
---
+++ main.py
@@ -0,0 +1,50 @@
+import pygame
+import math
+from Cell import Cell
+import tkinter as tk
+from tkinter import *
+
+# root = tk.Tk()
+# root.title("Path finding")
+# main_frame = tk.Frame(root, width=802, height=1002)
+# stat_frame = tk.Frame(main_frame,width=802, height=1002 - 802)
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 800
+WIN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Maze Generator")
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+w = 40
+# if one '/' is used the following error would occur
+# float' object cannot be interpreted as an integer
+
+
+grid = []
+visitedCell = None
+
+
+def make_grid():
+    rows = math.floor(WINDOW_WIDTH // w)
+    cols = math.floor(WINDOW_HEIGHT // w)
+    for i in range(rows):
+        for j in range(cols):
+            c = Cell(i, j, w)
+            grid.append(c)
+
+
+def main():
+    done = False
+    make_grid()
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            for c in grid:
+                c.draw(WIN)
+
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
