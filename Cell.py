c2d5d94ebb029b3e6543622a38f94be8bcec0c59
reza <reza.amjad65@gmail.com>
2021-05-13 17:45:57 -0700

    finding next move and coloring complete

--- Cell.py
+++ Cell.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -6,6 +7,7 @@ BLUE = (0, 255, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 PURPLE = (128, 0, 128)
+BLACK = (0, 0, 0)
 ORANGE = (255, 165, 0)
 TURQUOISE = (64, 224, 208)
 
@@ -17,9 +19,10 @@ class Cell:
         self.col = col
         self.x = row * width
         self.y = col * width
-        self.color = WHITE
+        self.color = BLACK
         self.width = width
         self.walls = [True, True, True, True]
+        self.neighbors = []
         self.visited = False
 
     def draw(self, win):
@@ -39,3 +42,32 @@ class Cell:
             pygame.draw.line(win, self.color, (self.x + self.width, self.y),
                              (self.x + self.width, self.y + self.width))
 
+    def find_neighbors(self, grid, rows, cols):
+        topx = self.row - 1
+        bottomx = self.row + 1
+        lefty = self.col - 1
+        righty = self.col + 1
+        # top
+        if not topx < 0:
+            self.neighbors.append(grid[topx][self.col])
+        # bottom
+        if not bottomx > rows - 1:
+            self.neighbors.append(grid[bottomx][self.col])
+        # left
+        if not lefty < 0:
+            self.neighbors.append(grid[self.row][lefty])
+        # right
+        if not righty > cols - 1:
+            self.neighbors.append(grid[self.row][righty])
+
+    def next_move(self):
+        if len(self.neighbors) > 0:
+            return self.neighbors[random.randint(0, len(self.neighbors) - 1)]
+
+
+    def visited_location(self, win):
+        self.visited = True
+        pygame.draw.rect(win, YELLOW, [self.x, self.y, self.width, self.width])
+
+    def __lt__(self, other):
+        return False
--- main.py
+++ main.py
@@ -1,50 +1,68 @@
 import pygame
 import math
 from Cell import Cell
-import tkinter as tk
-from tkinter import *
 
-# root = tk.Tk()
-# root.title("Path finding")
-# main_frame = tk.Frame(root, width=802, height=1002)
-# stat_frame = tk.Frame(main_frame,width=802, height=1002 - 802)
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+TURQUOISE = (64, 224, 208)
+BLACK = (0, 0, 0)
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 800
 WIN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+WIN.fill(WHITE)
 pygame.display.set_caption("Maze Generator")
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+
+
 w = 40
 # if one '/' is used the following error would occur
 # float' object cannot be interpreted as an integer
+rows = math.floor(WINDOW_WIDTH // w)
+cols = math.floor(WINDOW_HEIGHT // w)
 
 
-grid = []
+visited_list = []
 
-
-visited = None
 
 def make_grid():
-    rows = math.floor(WINDOW_WIDTH // w)
-    cols = math.floor(WINDOW_HEIGHT // w)
+    g = []
+
     for i in range(rows):
+        g.append([])
         for j in range(cols):
             c = Cell(i, j, w)
-            grid.append(c)
+            g[i].append(c)
+    return g
 
 
 def main():
     done = False
-    make_grid()
+    grid = make_grid()
+
+    fc = grid[0][0]
+    print(fc.row , fc.col)
+
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-            for c in grid:
-                c.draw(WIN)
+            for i in range(cols):
+                for c in grid[i]:
+                   c.draw(WIN)
 
         pygame.display.update()
+        fc.visited_location(WIN)
+        fc.find_neighbors(grid, rows, cols)
+        next_move = fc.next_move()
+        fc = next_move
+        fc.visited_location(WIN)
+        fc.find_neighbors(grid, rows, cols)
 
 
 if __name__ == "__main__":
